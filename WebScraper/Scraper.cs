6de0e758d6b434817d60b7db0abf723c32398d23
Vladislav <tsekov.vladislav@gmail.com>
2023-08-22 23:58:13 +0300

    Fixed spacing.

--- WebScraper/Scraper.cs
+++ WebScraper/Scraper.cs
@@ -1,4 +1,5 @@
-using HtmlAgilityPack;
+﻿using HtmlAgilityPack;
+using System.Text;
 using System.Text.RegularExpressions;
 
 class Program
@@ -15,94 +16,94 @@ class Program
 
         // Create an instance of HttpClient
         using HttpClient client = new();
-            try
-            {
+        try
+        {
             // Send an HTTP request to the URL
-                HttpResponseMessage response = await client.GetAsync(url);
+            HttpResponseMessage response = await client.GetAsync(url);
 
-                if (response.IsSuccessStatusCode)
-                {
-                    Stream contentStream = await response.Content.ReadAsStreamAsync();
+            if (response.IsSuccessStatusCode)
+            {
+                Stream contentStream = await response.Content.ReadAsStreamAsync();
 
-                    // Read the stream with the correct encoding (windows-1251 for Cyrillic)
+                // Read the stream with the correct encoding (windows-1251 for Cyrillic)
                 using StreamReader reader = new(contentStream, Encoding.GetEncoding("windows-1251"));
 
-                        string htmlContent = await reader.ReadToEndAsync();
+                string htmlContent = await reader.ReadToEndAsync();
 
-                        // Parse the HTML content using HtmlAgilityPack
+                // Parse the HTML content using HtmlAgilityPack
                 HtmlDocument doc = new();
-                        doc.LoadHtml(htmlContent);
+                doc.LoadHtml(htmlContent);
 
                 // Extract the motorcycle title using XPath
-                        var nameNodes = doc.DocumentNode.SelectNodes("//a[@class='mmm']");
+                var nameNodes = doc.DocumentNode.SelectNodes("//a[@class='mmm']");
 
-                        if (nameNodes != null)
+                if (nameNodes != null)
+                {
+                    foreach (var nameNode in nameNodes)
+                    {
+                        string name = nameNode.InnerText;
+                        if (string.IsNullOrEmpty(name))
                         {
-                            foreach (var nameNode in nameNodes)
-                            {
-                                string name = nameNode.InnerText;
-                                if (string.IsNullOrEmpty(name))
-                                {
-                                    continue;
-                                }
-                                Console.WriteLine($"Name: {name}");
-                            }
+                            continue;
                         }
-                        else
-                        {
-                            Console.WriteLine("No motorcycle names found on the page.");
-                        }
+                        Console.WriteLine($"Name: {name}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No motorcycle names found on the page.");
+                }
 
-                        // Extract the motorcycle prices using XPath
-                        var priceNodes = doc.DocumentNode.SelectNodes("//span[@class='price']");
+                // Extract the motorcycle prices using XPath
+                var priceNodes = doc.DocumentNode.SelectNodes("//span[@class='price']");
 
-                        if (priceNodes != null)
-                        {
-                            foreach (var priceNode in priceNodes)
-                            {
-                                string price = priceNode.InnerText;
-                                Console.WriteLine($"Price: {price}");
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine("No motorcycle prices found on the page.");
-                        }
+                if (priceNodes != null)
+                {
+                    foreach (var priceNode in priceNodes)
+                    {
+                        string price = priceNode.InnerText;
+                        Console.WriteLine($"Price: {price}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No motorcycle prices found on the page.");
+                }
 
                 // Extract the motorcycle year using XPath and regular expressions
                 var infoNodes = doc.DocumentNode.SelectNodes("//td[(contains(@colspan,'3') or contains(@colspan,'4')) and contains(@style,'padding-left:')]");
 
-                        if (infoNodes != null)
-                        {
-                            foreach (var infoNode in infoNodes)
-                            {
-                                string infoText = infoNode.InnerText;
-                                string yearPattern = @"\d{4}";
-                                Match yearMatch = Regex.Match(infoText, yearPattern);
+                if (infoNodes != null)
+                {
+                    foreach (var infoNode in infoNodes)
+                    {
+                        string infoText = infoNode.InnerText;
+                        string yearPattern = @"\d{4}";
+                        Match yearMatch = Regex.Match(infoText, yearPattern);
 
-                                if (yearMatch.Success)
-                                {
-                                    string year = yearMatch.Value;
-                                    Console.WriteLine($"Year: {year}");
-                                }
-                            }
-                        }
-                        else
+                        if (yearMatch.Success)
                         {
-                            Console.WriteLine("No motorcycle information found on the page.");
+                            string year = yearMatch.Value;
+                            Console.WriteLine($"Year: {year}");
                         }
                     }
+                }
                 else
                 {
-                    Console.WriteLine("Failed to retrieve the web page.");
+                    Console.WriteLine("No motorcycle information found on the page.");
                 }
             }
-            catch (Exception ex)
+            else
             {
-                Console.WriteLine($"An error occurred: {ex.Message}");
+                Console.WriteLine("Failed to retrieve the web page.");
             }
         }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
     }
+}
 
 
 
