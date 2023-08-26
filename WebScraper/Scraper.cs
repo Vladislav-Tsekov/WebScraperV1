37d56daf3fd9a31aee453e40864f6b467080e683
Vladislav <tsekov.vladislav@gmail.com>
2023-08-26 17:26:29 +0300

    Scraper can now Crawl, without locating "Next" button.

--- WebScraper/Scraper.cs
+++ WebScraper/Scraper.cs
@@ -7,130 +7,135 @@ class Scraper
 {
     static async Task Main(string[] args)
     {
-        // Register the 'windows-1251' encoding provider in order to read Cyrillic
+        // Register 'windows-1251' encoding provider in order to read Cyrillic
         Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-
-        // URL of the website to scrape
-        string url = "https://www.mobile.bg/pcgi/mobile.cgi?act=3&slink=thgxfy&f1=1";
-
-        // Insert a function that finds the "next" button - Web Crawler
 
         List<string[]> motorcycleTitles = new();
         List<int> motorcycleYears = new();
         List<int> motorcyclePrices = new();
 
-        // Create an instance of HttpClient
         using HttpClient client = new();
         try
         {
-            // Send an HTTP request to the URL
-            HttpResponseMessage response = await client.GetAsync(url);
+            // BASE URL IS USED BECAUSE THE URL STRUCTURE IS SIMILAR AND ONLY THE LAST SYMBOL (COUNTER) CHANGES
+            string baseURL = "https://www.mobile.bg/pcgi/mobile.cgi?act=3&slink=thgxfy&f1=";
+            
+            // MAX NUMBER OF PAGES TO SCRAPE
+            int maxPages = 9; // ADJUST THIS IF NEEDED, CURRENT NUMBER = 9 (26.08.2023)
 
-            if (response.IsSuccessStatusCode)
+            for (int i = 0; i < maxPages; i++)
             {
-                Stream contentStream = await response.Content.ReadAsStreamAsync();
+                string currentPageURL = "https://www.mobile.bg/pcgi/mobile.cgi?act=3&slink=thgxfy&f1=" + i;
 
-                // Read the stream with the correct encoding (windows-1251 for Cyrillic)
-                using StreamReader reader = new(contentStream, Encoding.GetEncoding("windows-1251"));
+                // Send an HTTP request to the URL
+                HttpResponseMessage response = await client.GetAsync(currentPageURL);
 
-                string htmlContent = await reader.ReadToEndAsync();
-
-                // Parse the HTML content
-                HtmlDocument doc = new();
-                doc.LoadHtml(htmlContent);
-
-                // Extract the motorcycle title using XPath
-                var titleNodes = doc.DocumentNode.SelectNodes("//a[@class='mmm']");
-
-                if (titleNodes != null)
+                if (response.IsSuccessStatusCode)
                 {
-                    foreach (var titleNode in titleNodes)
+                    Stream contentStream = await response.Content.ReadAsStreamAsync();
+
+                    // Read the stream with the correct encoding (windows-1251 for Cyrillic)
+                    using StreamReader reader = new(contentStream, Encoding.GetEncoding("windows-1251"));
+
+                    string htmlContent = await reader.ReadToEndAsync();
+
+                    // Parse the HTML content
+                    HtmlDocument doc = new();
+                    doc.LoadHtml(htmlContent);
+
+                    // Extract the motorcycle title using XPath
+                    var titleNodes = doc.DocumentNode.SelectNodes("//a[@class='mmm']");
+
+                    if (titleNodes != null)
                     {
-                        string title = titleNode.InnerText;
-
-                        // Defined regex patterns to capture the make, model and cc - separately
-
-                        string makePattern = @"^(.*?)(?:\s+|$)"; // Find the make (Ktm, Yamaha, etc)
-                        string modelPattern = @"(?:\s+|^)(.*?)(?:\s+\d+|$)"; // Find the model (EXC, WR, etc)
-                        string ccPattern = @"(?:\s+|^)(\d+)?(?:\s+|$)"; // Find the engine size (250, 300, etc)
-
-                        Match makeMatch = Regex.Match(title, makePattern);
-                        Match modelMatch = Regex.Match(title, modelPattern);
-                        Match ccMatch = Regex.Match(title, ccPattern);
-
-                        string make = makeMatch.Success ? makeMatch.Groups[1].Value.Trim() : "";
-                        string model = modelMatch.Success ? modelMatch.Groups[1].Value.Trim() : "";
-                        string cc = ccMatch.Success ? ccMatch.Groups[1].Value.Trim() : "N/A";
-
-                        if (string.IsNullOrEmpty(make))
+                        foreach (var titleNode in titleNodes)
                         {
-                            continue;
-                        }
+                            string title = titleNode.InnerText;
 
-                        model = model.Replace(make, "").Trim();
-                        int firstSpaceIndex = model.IndexOf(" ");
-                        if (firstSpaceIndex >= 0)
+                            // Defined regex patterns to capture the make, model and cc - separately
+
+                            string makePattern = @"^(.*?)(?:\s+|$)"; // Find the make (Ktm, Yamaha, etc)
+                            string modelPattern = @"(?:\s+|^)(.*?)(?:\s+\d+|$)"; // Find the model (EXC, WR, etc)
+                            string ccPattern = @"(?:\s+|^)(\d+)?(?:\s+|$)"; // Find the engine size (250, 300, etc)
+
+                            Match makeMatch = Regex.Match(title, makePattern);
+                            Match modelMatch = Regex.Match(title, modelPattern);
+                            Match ccMatch = Regex.Match(title, ccPattern);
+
+                            string make = makeMatch.Success ? makeMatch.Groups[1].Value.Trim() : "";
+                            string model = modelMatch.Success ? modelMatch.Groups[1].Value.Trim() : "";
+                            string cc = ccMatch.Success ? ccMatch.Groups[1].Value.Trim() : "N/A";
+
+                            if (string.IsNullOrEmpty(make))
+                            {
+                                continue;
+                            }
+
+                            model = model.Replace(make, "").Trim();
+                            int firstSpaceIndex = model.IndexOf(" ");
+                            if (firstSpaceIndex >= 0)
+                            {
+                                model = model.Substring(0, firstSpaceIndex);
+                            }
+
+                            string[] motoTitle = { make, model, cc };
+
+                            motorcycleTitles.Add(motoTitle);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine(string.Format(Utilities.NoTitleFound));
+                    }
+
+                    // Extract the motorcycle prices using XPath
+                    var priceNodes = doc.DocumentNode.SelectNodes("//span[@class='price']");
+
+                    if (priceNodes != null)
+                    {
+                        foreach (var priceNode in priceNodes)
                         {
-                            model = model.Substring(0, firstSpaceIndex);
+                            string priceInnerText = priceNode.InnerText;
+                            string price = Regex.Replace(priceInnerText, @"[^\d]", ""); // SHOULD REMOVE ".ЛВ" AT THE END
+                            //Console.WriteLine($"Price: {price}");
+                            int convertedPrice = int.Parse(price);
+                            motorcyclePrices.Add(convertedPrice);
                         }
+                    }
+                    else
+                    {
+                        Console.WriteLine(string.Format(Utilities.NoPriceFound));
+                    }
 
-                        string[] motoTitle = { make, model, cc }; 
+                    // Extract the motorcycle year using XPath and regular expressions
+                    var infoNodes = doc.DocumentNode.SelectNodes("//td[(contains(@colspan,'3') or contains(@colspan,'4')) and contains(@style,'padding-left:')]");
 
-                        motorcycleTitles.Add(motoTitle);
+                    if (infoNodes != null)
+                    {
+                        foreach (var infoNode in infoNodes)
+                        {
+                            string infoText = infoNode.InnerText;
+                            string yearPattern = @"\d{4}";
+                            Match yearMatch = Regex.Match(infoText, yearPattern);
+
+                            if (yearMatch.Success)
+                            {
+                                string year = yearMatch.Value;
+                                int convertedYear = int.Parse(year);
+                                //Console.WriteLine($"Year: {year}");
+                                motorcycleYears.Add(convertedYear);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine(string.Format(Utilities.NoYearFound));
                     }
                 }
                 else
                 {
-                    Console.WriteLine(string.Format(Utilities.NoTitleFound));
+                    Console.WriteLine(string.Format(Utilities.WebpageFailed));
                 }
-
-                // Extract the motorcycle prices using XPath
-                var priceNodes = doc.DocumentNode.SelectNodes("//span[@class='price']");
-
-                if (priceNodes != null)
-                {
-                    foreach (var priceNode in priceNodes)
-                    {
-                        string priceInnerText = priceNode.InnerText;
-                        string price = Regex.Replace(priceInnerText, @"[^\d]", ""); // SHOULD REMOVE ".ЛВ" AT THE END
-                        //Console.WriteLine($"Price: {price}");
-                        int convertedPrice = int.Parse(price);
-                        motorcyclePrices.Add(convertedPrice);
-                    }
-                }
-                else
-                {
-                    Console.WriteLine(string.Format(Utilities.NoPriceFound));
-                }
-
-                // Extract the motorcycle year using XPath and regular expressions
-                var infoNodes = doc.DocumentNode.SelectNodes("//td[(contains(@colspan,'3') or contains(@colspan,'4')) and contains(@style,'padding-left:')]");
-
-                if (infoNodes != null)
-                {
-                    foreach (var infoNode in infoNodes)
-                    {
-                        string infoText = infoNode.InnerText;
-                        string yearPattern = @"\d{4}";
-                        Match yearMatch = Regex.Match(infoText, yearPattern);
-
-                        if (yearMatch.Success)
-                        {
-                            string year = yearMatch.Value;
-                            int convertedYear = int.Parse(year);
-                            //Console.WriteLine($"Year: {year}");
-                            motorcycleYears.Add(convertedYear);
-                        }
-                    }
-                }
-                else
-                {
-                    Console.WriteLine(string.Format(Utilities.NoYearFound));
-                }
-            }
-            else
-            {
-                Console.WriteLine(string.Format(Utilities.WebpageFailed));
             }
         }
         catch (Exception ex)
@@ -138,14 +143,15 @@ class Scraper
             Console.WriteLine($"An error occurred: {ex.Message}");
         }
 
-        List<Motorcycle> motorcycles = new();
+        List<Motorcycle> currPageMotorcycles = new();
 
         // Print command for testing the output and merging collections
 
         for (int i = 0; i < motorcycleTitles.Count; i++)
         {
             var currentMotorcycle = new Motorcycle(motorcycleTitles[i][0], motorcycleTitles[i][1], motorcycleTitles[i][2], motorcycleYears[i], motorcyclePrices[i]);
-            motorcycles.Add(currentMotorcycle);
+
+            currPageMotorcycles.Add(currentMotorcycle);
 
             Console.WriteLine($"{i+1}. Make: {motorcycleTitles[i][0]}, Model: {motorcycleTitles[i][1]}, CC: {motorcycleTitles[i][2]}, Year: {motorcycleYears[i]}, Price: {motorcyclePrices[i]}");
         }
