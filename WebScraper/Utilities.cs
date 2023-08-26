8f8323e8e4df4ec27497869822660dfcdcdc4dd2
Vladislav <tsekov.vladislav@gmail.com>
2023-08-26 15:10:04 +0300

    Added "Utilities" class and created output/exception messages.

---
+++ WebScraper/Utilities.cs
@@ -0,0 +1,16 @@
+﻿namespace WebScraper
+{
+    public class Utilities
+    {
+        // OUTPUT MESSAGES
+
+        private const string NoTitleFound = "Error finding motorcycle titles!";
+        private const string NoPriceFound = "Error finding motorcycle prices!";
+        private const string NoYearFound = "Error finding motorcycle years!";
+
+        // EXCEPTION MESSAGES 
+
+        private const string MakeIsNullOrEmpty = "Exception 1: Make seems to be null or empty!";
+        private const string ModelIsNullOrEmpty = "Exception 2: Model seems to be null or empty!";
+    }
+}
