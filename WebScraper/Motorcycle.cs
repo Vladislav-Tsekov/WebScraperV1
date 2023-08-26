2b19712b2ed5a33eed5ee15afc22723eb811bb86
Vladislav <tsekov.vladislav@gmail.com>
2023-08-26 14:46:07 +0300

    Added properties in the Motorcycle class.

--- WebScraper/Motorcycle.cs
+++ WebScraper/Motorcycle.cs
@@ -2,5 +2,40 @@
 {
     public class Motorcycle
     {
-	}
+        private string? make;
+        private string? model;
+        private int cc;
+        private int year;
+        private int price;
+
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public int CC
+        {
+            get { return cc; }
+            set { cc = value; }
+        }
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        public int Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+    }
 }
