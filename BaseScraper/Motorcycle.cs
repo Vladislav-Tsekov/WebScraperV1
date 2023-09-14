3bb7511891575a1eb6568cb44192dac8d59538ca
Vladislav <tsekov.vladislav@gmail.com>
2023-09-14 19:44:23 +0300

    Implemented a "Motorcycle" class for the BaseScraper.

---
+++ BaseScraper/Motorcycle.cs
@@ -0,0 +1,27 @@
+﻿namespace BaseScraper
+{
+    public class Motorcycle
+    {
+		private string title;
+		private string year;
+		private string price;
+
+		public string Title
+		{
+			get { return title; }
+			set { title = value; }
+		}
+
+        public string Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        public string Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+    }
+}
