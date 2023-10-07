a86806bca9a1febca3967811be278407b02df722
Vladislav <tsekov.vladislav@gmail.com>
2023-10-07 23:10:09 +0300

    Implemented Motorcycle class for V1.2.

---
+++ TestScrapeV1.2-SC/Motorcycle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestScrapeV1._2_SC
+{
+    public class Motorcycle
+    {
+        public string Make { get; set; }
+        public string CC { get; set; }
+        public string Year { get; set; }
+        public double Price { get; set; }
+        public string AnnouncementLink { get; set; }
+    }
+}
