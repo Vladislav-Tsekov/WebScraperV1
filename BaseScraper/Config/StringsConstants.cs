647275bd373782322f21b2608500475552863797
Vladislav <tsekov.vladislav@gmail.com>
2023-12-04 20:32:25 +0200

    Converted "StringsConstants" namespace to a file-scoped one.

--- BaseScraper/Config/StringsConstants.cs
+++ BaseScraper/Config/StringsConstants.cs
@@ -1,37 +1,36 @@
-﻿namespace BaseScraper.Config
+﻿namespace BaseScraper.Config;
+
+public static class StringsConstants
 {
-    public static class StringsConstants
-    {
-        //Config
-        public const string AppSettingsPath = "appsettings.json";
+    //Config
+    public const string AppSettingsPath = "appsettings.json";
 
-        //Base URLs
-        public const string MxBaseUrl = "https://www.mobile.bg/pcgi/mobile.cgi?act=3&slink=umq0fu&f1=";
-        public const string EnduroBaseUrl = "https://www.mobile.bg/pcgi/mobile.cgi?act=3&slink=umpnpb&f1=";
+    //Base URLs
+    public const string MxBaseUrl = "https://www.mobile.bg/pcgi/mobile.cgi?act=3&slink=umq0fu&f1=";
+    public const string EnduroBaseUrl = "https://www.mobile.bg/pcgi/mobile.cgi?act=3&slink=umpnpb&f1=";
 
-        //Encoding
-        public const string Encoding = "windows-1251";
+    //Encoding
+    public const string Encoding = "windows-1251";
 
-        //Nodes
-        public const string TitleNodes = "//a[@class='mmm']";
-        public const string PriceNodes = "//span[@class='price']";
-        public const string DescriptionNodes = "//td[(contains(@colspan,'3') or contains(@colspan,'4')) and contains(@style,'padding-left:')]";
-        public const string LinkNodes = "//a[@class='mmm']";
-        public const string HrefAttribute = "href";
-        public const string HrefDefault = "";
+    //Nodes
+    public const string TitleNodes = "//a[@class='mmm']";
+    public const string PriceNodes = "//span[@class='price']";
+    public const string DescriptionNodes = "//td[(contains(@colspan,'3') or contains(@colspan,'4')) and contains(@style,'padding-left:')]";
+    public const string LinkNodes = "//a[@class='mmm']";
+    public const string HrefAttribute = "href";
+    public const string HrefDefault = "";
 
-        //Output
-        public const string NotAvailable = "N/A";
-        public const string NoTitlesFound = "No titles found on the page.";
-        public const string NoPricesFound = "No prices found on the page.";
-        public const string NoYearsFound = "No years found on the page.";
-        public const string NoLinksFound = "No matching links found.";
-        public const string FailedToRetreivePage = "Failed to retrieve the web page.";
+    //Output
+    public const string NotAvailable = "N/A";
+    public const string NoTitlesFound = "No titles found on the page.";
+    public const string NoPricesFound = "No prices found on the page.";
+    public const string NoYearsFound = "No years found on the page.";
+    public const string NoLinksFound = "No matching links found.";
+    public const string FailedToRetreivePage = "Failed to retrieve the web page.";
 
-        //RegEx
-        public const string CcPattern = @"\d{3}";
-        public const string PriceIdentify = @"[^\d]";
-        public const string PriceReplace = "";
-        public const string YearPattern = @"\d{4}";
-    }
+    //RegEx
+    public const string CcPattern = @"\d{3}";
+    public const string PriceIdentify = @"[^\d]";
+    public const string PriceReplace = "";
+    public const string YearPattern = @"\d{4}";
 }
