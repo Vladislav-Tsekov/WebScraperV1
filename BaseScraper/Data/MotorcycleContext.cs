e2423ff3e4263e90454706c98310904a97bf7456
Vladislav <tsekov.vladislav@gmail.com>
2023-12-04 21:39:00 +0200

    Created the FluentAPI default class - MotorcycleContext.

---
+++ BaseScraper/Data/MotorcycleContext.cs
@@ -0,0 +1,26 @@
+﻿using BaseScraper.Data;
+using BaseScraper.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebScraperV1.Data
+{
+
+    public class MotorcycleContext : DbContext
+    {
+        public MotorcycleContext(DbContextOptions options) : base(options) {}
+
+        public DbSet<MotorcycleMake> Makes { get; set; }
+        public DbSet<MotorcycleYear> Years { get; set; }
+        public DbSet<MotocrossMarketPrice> ListOfPrices { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer(DatabaseSettings.ConnectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
