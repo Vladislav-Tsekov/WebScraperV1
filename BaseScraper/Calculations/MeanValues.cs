2b2f0a189c018fa69d9ff1781c1d41ecfdafdac9
Vladislav <tsekov.vladislav@gmail.com>
2023-12-21 23:01:01 +0200

    Implemented a method for median value calculation.

--- BaseScraper/Calculations/MeanValues.cs
+++ BaseScraper/Calculations/MeanValues.cs
@@ -5,15 +5,15 @@
         public const double trimPercentage = 0.20;
         public const double deviationThreshold = 1;
 
-        public static double Dev(IEnumerable<double> data, double deviationThreshold)
+        public static double Dev(IEnumerable<double> prices, double deviationThreshold)
         {
-            double[] dataArray = data.ToArray();
-            double mean = dataArray.Average();
+            double[] pricesArray = prices.ToArray();
+            double mean = pricesArray.Average();
 
-            double standardDeviation = Math.Sqrt(dataArray.Select(x => Math.Pow(x - mean, 2)).Average());
+            double standardDeviation = Math.Sqrt(pricesArray.Select(x => Math.Pow(x - mean, 2)).Average());
             double deviationLimit = deviationThreshold * standardDeviation;
 
-            double[] trimmedData = dataArray.Where(x => Math.Abs(x - mean) <= deviationLimit).ToArray();
+            double[] trimmedData = pricesArray.Where(x => Math.Abs(x - mean) <= deviationLimit).ToArray();
             double trimmedMean = trimmedData.Average();
 
             return trimmedMean;
@@ -30,6 +30,25 @@
             double trimmedMean = trimmedData.Average();
 
             return trimmedMean;
+        }
+
+        //TODO - TEST, POLISH THEN IMPLEMENT - Middle or median value. Case handling - single motorcycle.
+        public static double Median(IEnumerable<double> prices)
+        {
+            double[] pricesArray = prices.ToArray();
+            Array.Sort(pricesArray);
+
+            if (pricesArray.Length % 2 == 0)
+            {
+                int middleIndex1 = pricesArray.Length / 2 - 1;
+                int middleIndex2 = pricesArray.Length / 2;
+                return (pricesArray[middleIndex1] + pricesArray[middleIndex2]) / 2.0;
+            }
+            else
+            {
+                int middleIndex = pricesArray.Length / 2;
+                return pricesArray[middleIndex];
+            }
         }
     }
 }
--- BaseScraper/obj/Debug/net6.0/BaseScraper.AssemblyInfo.cs
+++ BaseScraper/obj/Debug/net6.0/BaseScraper.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaseScraper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6739124fe5b1865a833e15a81c97e338a47d585")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaseScraper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaseScraper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
