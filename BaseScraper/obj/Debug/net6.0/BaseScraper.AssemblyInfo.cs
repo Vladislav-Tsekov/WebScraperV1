43fc574c0974fe6fefd983d8090102f677738a1d
Vladislav <tsekov.vladislav@gmail.com>
2023-12-22 14:51:23 +0200

    Implemented a new class - DataExport and moved part of the logic.

--- BaseScraper/obj/Debug/net6.0/BaseScraper.AssemblyInfo.cs
+++ BaseScraper/obj/Debug/net6.0/BaseScraper.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaseScraper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f328f4b503835452c1aaedcebd165472c6df1b32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6319346c6e79aafc40343293afdec0cbacdbb2f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaseScraper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaseScraper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
