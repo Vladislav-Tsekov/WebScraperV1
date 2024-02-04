d4046d982cade2a9e4aab74c7fa57e79f66e8c68
Vladislav <tsekov.vladislav@gmail.com>
2024-02-04 21:41:32 +0200

    Updated database contents. Manually removed false data.

---
+++ BaseScraper/Migrations/20240204193959_AddedComments.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BaseScraper.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedComments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterTable(
+                name: "MotocrossSoldEntries",
+                comment: "A table of all sold motorcycles");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Price",
+                table: "MotocrossSoldEntries",
+                type: "float",
+                nullable: false,
+                comment: "Motorcycle's price",
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateSold",
+                table: "MotocrossSoldEntries",
+                type: "datetime2",
+                nullable: false,
+                comment: "Date of announcement's removal from the website",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateAdded",
+                table: "MotocrossSoldEntries",
+                type: "datetime2",
+                nullable: false,
+                comment: "Date of announcement's addition to the database",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cc",
+                table: "MotocrossSoldEntries",
+                type: "nvarchar(max)",
+                nullable: true,
+                comment: "Motorcycle's engine displacement",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "StdDevPrice",
+                table: "MotocrossMarketPrices",
+                type: "decimal(18,2)",
+                nullable: false,
+                comment: "The Standard Deviation Price for each make/year combination",
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PriceVariance",
+                table: "MotocrossMarketPrices",
+                type: "decimal(18,2)",
+                nullable: false,
+                comment: "The Price Variance coefficient for each make/year combination",
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PriceRange",
+                table: "MotocrossMarketPrices",
+                type: "decimal(18,2)",
+                nullable: false,
+                comment: "The Price Range (most expensive - cheapest announcement) for each make/year combination",
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MotoCount",
+                table: "MotocrossMarketPrices",
+                type: "int",
+                nullable: false,
+                comment: "The number of motorcycle announcements for the current make/year combination",
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ModePrice",
+                table: "MotocrossMarketPrices",
+                type: "decimal(18,2)",
+                nullable: false,
+                comment: "The Mode Price (most frequent value, if such exists) for each make/year combination",
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "MedianPrice",
+                table: "MotocrossMarketPrices",
+                type: "decimal(18,2)",
+                nullable: false,
+                comment: "The Median Price for each make/year combination",
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "MeanTrimPrice",
+                table: "MotocrossMarketPrices",
+                type: "decimal(18,2)",
+                nullable: false,
+                comment: "The Mean Price for each make/year combination, calculated with a trim factor of 0.20",
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "AvgPrice",
+                table: "MotocrossMarketPrices",
+                type: "decimal(18,2)",
+                nullable: false,
+                comment: "The Average Price for each make/year combination",
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterTable(
+                name: "MotocrossSoldEntries",
+                oldComment: "A table of all sold motorcycles");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Price",
+                table: "MotocrossSoldEntries",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldComment: "Motorcycle's price");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateSold",
+                table: "MotocrossSoldEntries",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldComment: "Date of announcement's removal from the website");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateAdded",
+                table: "MotocrossSoldEntries",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldComment: "Date of announcement's addition to the database");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cc",
+                table: "MotocrossSoldEntries",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true,
+                oldComment: "Motorcycle's engine displacement");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "StdDevPrice",
+                table: "MotocrossMarketPrices",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldComment: "The Standard Deviation Price for each make/year combination");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PriceVariance",
+                table: "MotocrossMarketPrices",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldComment: "The Price Variance coefficient for each make/year combination");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PriceRange",
+                table: "MotocrossMarketPrices",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldComment: "The Price Range (most expensive - cheapest announcement) for each make/year combination");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MotoCount",
+                table: "MotocrossMarketPrices",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldComment: "The number of motorcycle announcements for the current make/year combination");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ModePrice",
+                table: "MotocrossMarketPrices",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldComment: "The Mode Price (most frequent value, if such exists) for each make/year combination");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "MedianPrice",
+                table: "MotocrossMarketPrices",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldComment: "The Median Price for each make/year combination");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "MeanTrimPrice",
+                table: "MotocrossMarketPrices",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldComment: "The Mean Price for each make/year combination, calculated with a trim factor of 0.20");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "AvgPrice",
+                table: "MotocrossMarketPrices",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldComment: "The Average Price for each make/year combination");
+        }
+    }
+}
--- BaseScraper/obj/Debug/net6.0/BaseScraper.AssemblyInfo.cs
+++ BaseScraper/obj/Debug/net6.0/BaseScraper.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaseScraper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4354d55edcb2e407bd2b79a2e2c3c4446668285f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+731cac3174461cfe94df088721a0b2d06788e1da")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaseScraper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaseScraper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
