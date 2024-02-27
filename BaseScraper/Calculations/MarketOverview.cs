577d0b2e27ab31bc361d7d1cc46a95529382a922
Vladislav <tsekov.vladislav@gmail.com>
2024-02-27 11:42:37 +0200

    Deleted existing data and migrations. Reseted the database.

--- BaseScraper/Calculations/MarketOverview.cs
+++ BaseScraper/Calculations/MarketOverview.cs
@@ -1,4 +1,5 @@
 ﻿using BaseScraper.Data.Models;
+using System.Text;
 
 namespace BaseScraper.Calculations
 {
@@ -6,22 +7,37 @@ namespace BaseScraper.Calculations
     {
         public static void MarketShareByEngineDisplacement(HashSet<MotocrossEntry> entriesSet, StreamWriter marketWriter) 
         {
-            double totalEntries = entriesSet.Count;
-            double countOf250s = entriesSet.Where(m => m.Cc == 250).Count();
-            double countOf350s = entriesSet.Where(m => m.Cc == 350).Count();
-            double countOf450s = entriesSet.Where(m => m.Cc == 450).Count();
-            double unknownCcCount = entriesSet.Where(m => m.Cc == 0).Count();
-            double existingCcCount = totalEntries - unknownCcCount;
+            //double totalEntries = entriesSet.Count;
+            //double countOf250s = entriesSet.Where(m => m.Cc == 250).Count();
+            //double countOf350s = entriesSet.Where(m => m.Cc == 350).Count();
+            //double countOf450s = entriesSet.Where(m => m.Cc == 450).Count();
+            //double unknownCcCount = entriesSet.Where(m => m.Cc == 0).Count();
+            //double existingCcCount = totalEntries - unknownCcCount;
 
-            marketWriter.WriteLine($"There are currently {totalEntries} Motocross announcements.");
-            marketWriter.WriteLine($"{countOf250s} out of {totalEntries} are 250 cc.");
-            marketWriter.WriteLine($"{countOf350s} out of {totalEntries} are 350 cc.");
-            marketWriter.WriteLine($"{countOf450s} out of {totalEntries} are 450 cc.");
-            marketWriter.WriteLine($"{unknownCcCount} out of {totalEntries} are neither of the above or do not contain engine displacement information.");
-            marketWriter.WriteLine($"Market Share (CC, %):");
-            marketWriter.WriteLine($"250, {(countOf250s / existingCcCount) * 100:f2}");
-            marketWriter.WriteLine($"350, {(countOf350s / existingCcCount) * 100:f2}");
-            marketWriter.WriteLine($"450, {(countOf450s / existingCcCount) * 100:f2}");
+            //marketWriter.WriteLine($"There are currently {totalEntries} Motocross announcements.");
+            //marketWriter.WriteLine($"{countOf250s} out of {totalEntries} are 250 cc.");
+            //marketWriter.WriteLine($"{countOf350s} out of {totalEntries} are 350 cc.");
+            //marketWriter.WriteLine($"{countOf450s} out of {totalEntries} are 450 cc.");
+            //marketWriter.WriteLine($"{unknownCcCount} out of {totalEntries} are neither of the above or do not contain engine displacement information.");
+            //marketWriter.WriteLine($"Market Share (CC, %):");
+            //marketWriter.WriteLine($"250, {(countOf250s / existingCcCount) * 100:f2}");
+            //marketWriter.WriteLine($"350, {(countOf350s / existingCcCount) * 100:f2}");
+            //marketWriter.WriteLine($"450, {(countOf450s / existingCcCount) * 100:f2}");
+
+            int totalEntries = entriesSet.Count;
+            var ccCounts = entriesSet.GroupBy(m => m.Cc).ToDictionary(g => g.Key, g => g.Count());
+
+            StringBuilder sb = new();
+            sb.AppendLine($"There are currently {totalEntries} Motocross announcements.");
+
+            foreach (var ccEntry in ccCounts)
+            {
+                string cc = ccEntry.Key == 0 ? "unknown or missing" : ccEntry.Key.ToString();
+                double percentage = (double)ccEntry.Value / totalEntries * 100;
+                sb.AppendLine($"{ccEntry.Value} out of {totalEntries} are {cc} cc. ({percentage:f2}%)");
+            }
+
+            marketWriter.Write(sb.ToString());
         }
 
         public static void MarketShareByMakeAndYear(List<MotocrossMarketPrice> pricesList, StreamWriter marketWriter)
--- BaseScraper/Data/DatabaseSettings.cs
+++ BaseScraper/Data/DatabaseSettings.cs
@@ -4,6 +4,6 @@ namespace BaseScraper.Data
 {
     public class DatabaseSettings
     {
-        public const string ConnectionString = @"Server=(localdb)\MSSQLLocalDB;Database=MotoMarketPrices;Integrated Security=True";
+        public const string ConnectionString = @"Server=(localdb)\MSSQLLocalDB;Database=MotoMarketAnalysis;Integrated Security=True";
     }
 }
